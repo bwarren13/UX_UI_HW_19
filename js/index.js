ef4f9e672316cbfc6bcba68405f2f19d6e56dc52
bwarren13 <159840489+bwarren13@users.noreply.github.com>
2024-08-31 17:13:31 -0400

    Contact

--- js/index.js
+++ js/index.js
@@ -23,28 +23,4 @@ window.onclick = function(event) {
   }
 }
 
-var modal = document.getElementById("myModal");
-
-
-var btn = document.getElementById("#contact");
-
-
-var span = document.getElementsByClassName("close")[0];
-
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
